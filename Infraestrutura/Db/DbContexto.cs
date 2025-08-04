056af654d6429b916c757d59aaf7f1c4b3a872ea
Gustavo Fidelis <gustavo.camilo@polygonsolucoes.com.br>
2025-08-04 12:27:30 -0300

    feat: Iniciando projeto

---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace minimal_api.Infraestrutura.Db
+{
+    public class DbContexto : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
